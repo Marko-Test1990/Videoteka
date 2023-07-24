6b8f04e95bba8fa5091d0f59143e93a6fddab6f8
Marko <canadianlynx13@gmail.com>
2023-07-24 15:59:50 +0200

    Dodata osnovna strana Kupac i _Layout sminka

---
+++ Videoteka/Controllers/KupacController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Videoteka.Models;
+
+namespace Videoteka.Controllers
+{
+    public class KupacController : Controller
+    {
+        // GET: Kupac
+        public ActionResult Index()
+        {
+            var kupac = new Kupac() { Ime="Joe Smith"};
+
+            return View(kupac);
+        }
+    }
+}
---
+++ Videoteka/Models/Kupac.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Videoteka.Models
+{
+    public class Kupac
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+    }
+}
