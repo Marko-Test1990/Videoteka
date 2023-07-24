563c082b711bc25639dd208f725847c43324c046
Marko <canadianlynx13@gmail.com>
2023-07-24 15:48:11 +0200

    Dodata osnovna film/index strana

---
+++ Videoteka/Controllers/FilmController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Videoteka.Models;
+
+namespace Videoteka.Controllers
+{
+    public class FilmController : Controller
+    {
+        // GET: Film
+        public ActionResult Index()
+        {
+            var film = new Film() { Naziv = "Taxi" };
+
+            return View(film);
+        }
+    }
+}
