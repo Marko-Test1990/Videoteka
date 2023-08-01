bc5db432f80a96c795f96e6b43168e543e3274e8
Marko <canadianlynx13@gmail.com>
2023-08-01 05:49:40 +0200

    Delete linkovi za Filmove i Kupce

---
+++ Videoteka/Controllers/Api/FilmoviController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Videoteka.Models;
+
+namespace Videoteka.Controllers.Api
+{
+    public class FilmoviController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FilmoviController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        public IEnumerable<Film> GetFilmovi()
+        {
+            return _context.Films.ToList();
+        }
+
+        public IHttpActionResult GetFilm(int id)
+        {
+            var film = _context.Films.SingleOrDefault(c => c.Id == id);
+
+            if (film == null)
+                return NotFound();
+
+            return Ok(film);
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateFilm(Film film)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Films.Add(film);
+            _context.SaveChanges();
+            return Created(new Uri(Request.RequestUri + "/" + film.Id), film);
+        }
+        
+        [HttpDelete]
+        public IHttpActionResult DeleteFilmovi(int id)
+        {
+            var filmUDb = _context.Films.SingleOrDefault(c => c.Id == id);
+
+            if (filmUDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Films.Remove(filmUDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
---
+++ Videoteka/Controllers/Api/KupciController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Videoteka.Models;
+
+namespace Videoteka.Controllers.Api
+{
+    public class KupciController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public KupciController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        public IEnumerable<Kupac> GetKupci()
+        {
+            return _context.Kupci.ToList();
+        }
+
+        public IHttpActionResult GetKupac(int id)
+        {
+            var kupac = _context.Kupci.SingleOrDefault(c => c.Id == id);
+
+            if (kupac == null)
+                return NotFound();
+
+            return Ok(kupac);
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateKupac(Kupac kupac)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Kupci.Add(kupac);
+            _context.SaveChanges();
+            return Created(new Uri(Request.RequestUri + "/" + kupac.Id), kupac);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteKupci(int id)
+        {
+            var kupacUDb = _context.Kupci.SingleOrDefault(c => c.Id == id);
+
+            if (kupacUDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Kupci.Remove(kupacUDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
