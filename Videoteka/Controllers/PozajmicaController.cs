1d29d77947bee8da01ea28812728dec77e4b6d1d
Marko <canadianlynx13@gmail.com>
2023-08-01 02:44:29 +0200

    Osnovna Pozajmica strana

---
+++ Videoteka/Controllers/PozajmicaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using Videoteka.Models;
+
+namespace Videoteka.Controllers
+{
+    public class PozajmicaController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public PozajmicaController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Pozajmica  .Include(c => c.Kupac).Include(c => c.Film)
+        public ActionResult Index()
+        {
+            var pozajmica = _context.pozajmice.Include(c => c.Kupac).Include(c => c.Film).ToList();
+
+            return View(pozajmica);
+            
+        }
+
+       
+
+
+    }
+}
---
+++ Videoteka/ViewModels/PozajmicaFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Videoteka.Models;
+
+namespace Videoteka.ViewModels
+{
+    public class PozajmicaFormViewModel
+    {
+        public IEnumerable<Kupac> Kupci { get; set; }
+        public IEnumerable<Film> Filmovi { get; set; } 
+        public Pozajmica Pozajmica { get; set; }
+
+        public string naslov
+        {
+            get
+            {
+                if (Pozajmica != null && Pozajmica.Id != 0)
+                    return "Izmijeni Film";
+                else
+                    return "Novi Film";
+            }
+        }
+    }
+}
