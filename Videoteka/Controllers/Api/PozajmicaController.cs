4b78c393bccaf06d683a4d4b1e95e66e2dfe236a
Marko <canadianlynx13@gmail.com>
2023-08-01 05:13:13 +0200

    Pozajmica API i Delete opcija

---
+++ Videoteka/Controllers/Api/PozajmicaController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Videoteka.Models;
+
+namespace Videoteka.Controllers.Api
+{
+    public class PozajmicaController : ApiController
+    {   
+        private ApplicationDbContext _context;
+
+        public PozajmicaController() 
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Remove Later?
+        public IEnumerable<Pozajmica> GetPozajmice() 
+        {
+            return _context.pozajmice.ToList();
+        }
+
+        //Remove Later?
+        public IHttpActionResult GetPozajmica(int id) 
+        {
+            var pozajmica = _context.pozajmice.SingleOrDefault(c  => c.Id == id);
+
+            if(pozajmica == null)
+                return NotFound();
+
+            return Ok(pozajmica);
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreatePozajmica(Pozajmica pozajmica) 
+        {
+            if(!ModelState.IsValid) 
+            {
+                return BadRequest();
+            }
+
+            _context.pozajmice.Add(pozajmica);
+            _context.SaveChanges();
+            return Created(new Uri(Request.RequestUri + "/" + pozajmica.Id),pozajmica);
+        }
+
+        //Remove Later?
+        /*public void UpdatePozajmica(int id, Pozajmica pozajmica) 
+        {
+            var pozajmicaUDb = _context.pozajmice.SingleOrDefault(c => c.Id == id);
+
+            if (pozajmicaUDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+
+        }*/
+
+        [HttpDelete]
+        public IHttpActionResult DeletePozajmica (int id) 
+        {
+            var pozajmicaUDb = _context.pozajmice.SingleOrDefault(c => c.Id == id);
+
+            if(pozajmicaUDb == null) 
+            {
+                return NotFound();
+            }
+
+            _context.pozajmice.Remove(pozajmicaUDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
